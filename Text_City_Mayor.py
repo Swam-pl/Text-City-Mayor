770ace2296ce1b42994515bc5e01e692872e3485
Swam-pl <94205675+Swam-pl@users.noreply.github.com>
2021-12-11 13:18:17 +0100

    Update Text_City_Mayor.py

--- Text_City_Mayor.py
+++ Text_City_Mayor.py
@@ -1,14 +1,47 @@
+import os
 import sqlite3
+import time
 
 def game():
-    def checkforsavefile():
-        conn=sqlite3.connect('savefile.db')
-        cur=conn.cursor()
-        conn.close()
+    def slowtext(text):
+        for letter in text:
+            print(letter,end='')
+            time.sleep(0.01)
+        return ''
             
-    def main():
-        while True:
+    ##########
+    
+    def data():
+        print(f'Mayor {name}! Here is the data about our city this year: ')
+    
+    def choosename():
+        name=input(slowtext('Congratulations on being elected as the mayor! What do you want to be known as? '))
+    
+    def savefile(order):
+        def readdata():
+            if(os.path.isfile('savefile.db')):
+                conn=sqlite3.connect('savefile.db')
+                cur=conn.cursor()
+                conn.close()
+            else:
+                answer=input(slowtext('savefile.db not found. Create new save file? y/n '))
+                if(answer.lower()=='y'):
+                    return 'sukces'
+                elif(answer.lower()=='n'):
+                    input(slowtext('Press any button to exit the game. '))
+                    return 'exit'
+                else:
+                    slowtext('Unkown answer. ')
+                    return readsavefile()
+        def savedata():
             pass
+        
+        return exec(order)
+        
+    def main():
+        data=savefile('readdata()')
+        if(data!='exit'):
+            print('x')
 
     main()
 

21cc1f45d76e680bdb5e471fd34355b3ee28c88f
Swam-pl <94205675+Swam-pl@users.noreply.github.com>
2021-12-27 18:23:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,80 @@
+##############################
+# 
+# Text City Mayor
+# Created by Tobiasz Duliniec aka Swam-pl
+#
+##############################
+
+
+from time import sleep
+import datetime
 import os
 import sqlite3
-import time
 
 def game():
     def slowtext(text):
+        '''
+        A function responsible for printing text letter by letter. It takes a text to print as an argument
+        and prints each letter of it every 0.01s. The function returns an empty string.
+        '''
         for letter in text:
             print(letter,end='')
-            time.sleep(0.01)
+            sleep(0.01)
         return ''
             
     ##########
     
-    def data(name):
-        slowtext(f'Mayor {name}! Here is the data about our city this year: ')
+    def data(data):
+        '''
+        A function called at the beginning of the every turn. It takes a tuple with all the game data as an arguments
+        and prints data from it.
+        '''
+        slowtext(f'Mayor {data[0]}! Here is the data about our city this year: ')
     
     def choosename():
-        name=input(slowtext('Congratulations on being elected as the mayor! How do you want to be known? '))
+        '''
+        A function called only if the player's name is not set. It takes no arguments and
+        returns a name supplied by the user.
+        '''
+        return input(slowtext('Congratulations on being elected as the mayor! How do you want to be known? '))
     
     def savefile(order):
         def readdata():
+            '''
+            A function responsible for reading data from savefile.db.
+            It checks if savefile.db exists and if it does it returns a tuple with the first record of it.
+            If not, it asks if savefile.db should be created and if so: creates a new file and returns itself.
+            If the player decides not to create a new file it prompts the user to click ENTER to leave the game and 
+            returns a string with a text of "exit".
+            If the players enters an unkown command the function will return itself.
+            '''
             if(os.path.isfile('savefile.db')):
                 conn=sqlite3.connect('savefile.db')
                 cur=conn.cursor()
+                data=tuple(cur.execute("SELECT * FROM savedata LIMIT 1;"))
                 conn.close()
+                return data
             else:
                 answer=input(slowtext('savefile.db not found. Create new save file? y/n '))
                 if(answer.lower()=='y'):
-                    return 'ok'
+                    conn=sqlite3.connect('safefile.db')
+                    cur=conn.cursor()
+                    cur.execute('''CREATE TABLE "savedata" (
+                                    "ID" INTEGER,
+                                    "username" TEXT,
+                                    "game_start_date" TEXT,
+                                    "in_game_date" TEXT,
+                                    "last_election_date" TEXT,
+                                    "money" INTEGER,
+                                    "population" INTEGER,
+                                    "respect" INTEGER,
+                                    "projected_support" INTEGER,
+                                    PRIMARYKEY("ID")
+                                )''')
+                    conn.commit()
+                    conn.close()
+                    slowtext('Save file (savefile.db) created succesfully.')
+                    return readdata()
                 elif(answer.lower()=='n'):
                     input(slowtext('Press ENTER to exit the game. '))
                     return 'exit'
@@ -42,7 +90,7 @@ def game():
     def main():
         data=savefile('readdata()')
         if(data!='exit'):
-            pass
+            print('not exit')
 
     main()
 

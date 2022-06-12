ae0d1ce574acb4e2ac1b81485f8ccf6796d3b4f4
Swam-pl <94205675+Swam-pl@users.noreply.github.com>
2022-06-12 13:10:19 +0200

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -11,23 +11,23 @@
 Various functions used by multiple files.
 '''
 
-from time import sleep
-
 try:
+    from time import sleep
     from winsound import Beep
 except ImportError:
     pass
 
 def slowtext(*args):
     '''
-    A function responsible for printing text letter by letter. It takes a text to print as an argument
-    and prints each letter of it every 0.01s. The function returns an empty string.
+    A function responsible for printing text letter by letter.
+    It takes texts to print as an argument and prints their every
+    letter every 0.01s. The function returns an empty string.
     '''
     for text in args:
         for letter in text:
             try:
                 Beep(1000,2)
-            except RuntimeError:
+            except (NameError,RuntimeError):
                 pass
             print(letter,end='')
             sleep(0.1)
@@ -35,6 +35,6 @@ def slowtext(*args):
 
 def errors(code,e):
     def databaseerror(e):
-        slowtext(f'Error 001: Database Error - refer to errors.txt for more information ({e}).')
+        slowtext(f'Error 001: Database Error - refer to manual.txt for more information ({e}).')
         
     exec(f'{code}(e)')
